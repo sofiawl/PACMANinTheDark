5665c8d9df3cbfd3e8e06260295bdc635fa69519
Sofia Lima <sofiawamserl@icloud.com>
2026-04-22 20:55:25 -0300

    feat: add ncurser

--- client_view.cpp
+++ client_view.cpp
@@ -30,13 +30,8 @@ void draw_client_view_from_csv(const char* csv_path) {
         return;
     }
 
-    // Fixed viewport in terminal character cells 
-    constexpr int VIEW_W = 40;
-    constexpr int VIEW_H = 40;
-    constexpr int CELL = 1;
-
-    const int MAP_W = SIZE_WORLD * CELL;
-    const int MAP_H = SIZE_WORLD * CELL;
+    const int map_w = SIZE_WORLD;
+    const int map_h = SIZE_WORLD;
 
     initscr();
     noecho();
@@ -47,52 +42,49 @@ void draw_client_view_from_csv(const char* csv_path) {
     int scr_h = 0, scr_w = 0;
     getmaxyx(stdscr, scr_h, scr_w);
 
-    if (scr_w < VIEW_W || scr_h < VIEW_H) {
+    if (scr_w < map_w || scr_h < map_h) {
         endwin();
         std::fprintf(
             stderr,
             "Terminal too small: need at least %d cols x %d rows (have %d x %d)\n",
-            VIEW_W,
-            VIEW_H,
+            map_w,
+            map_h,
             scr_w,
             scr_h);
         return;
     }
 
-    const int origin_y = (scr_h - VIEW_H) / 2;
-    const int origin_x = (scr_w - VIEW_W) / 2;
+    const int hint_h = (scr_h >= map_h + 1) ? 1 : 0;
+    const int win_w = map_w;
+    const int win_h = map_h + hint_h;
+
+    const int origin_y = (scr_h - win_h) / 2;
+    const int origin_x = (scr_w - win_w) / 2;
 
     erase();
-    WINDOW* box = newwin(VIEW_H, VIEW_W, origin_y, origin_x);
+    WINDOW* box = newwin(win_h, win_w, origin_y, origin_x);
     if (!box) {
         endwin();
         std::fprintf(stderr, "newwin failed\n");
         return;
     }
 
-    const int mx = (VIEW_W - MAP_W) / 2;
-    const int my = (VIEW_H - MAP_H) / 2;
-
     werase(box);
     for (int i = 0; i < SIZE_WORLD; ++i) {
         for (int j = 0; j < SIZE_WORLD; ++j) {
             char ch = world[i][j];
             if (ch == '0') ch = ' ';
-            const int base_r = my + i * CELL;
-            const int base_c = mx + j * CELL;
-            for (int dr = 0; dr < CELL; ++dr) {
-                for (int dc = 0; dc < CELL; ++dc) {
-                    mvwaddch(box, base_r + dr, base_c + dc, static_cast<unsigned char>(ch));
-                }
-            }
+            mvwaddch(box, i, j, static_cast<unsigned char>(ch));
         }
     }
 
-    const char* msg = "Press q to quit";
-    const int msglen = (int)std::strlen(msg);
-    int col = (VIEW_W - msglen) / 2;
-    if (col < 0) col = 0;
-    mvwprintw(box, VIEW_H - 1, col, "%s", msg);
+    if (hint_h) {
+        const char* msg = "q=quit";
+        const int msglen = (int)std::strlen(msg);
+        int col = (win_w - msglen) / 2;
+        if (col < 0) col = 0;
+        mvwprintw(box, map_h, col, "%s", msg);
+    }
 
     wrefresh(box);
     refresh();
