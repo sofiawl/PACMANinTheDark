10d4d29ff1c9511fa1b219bc98db72f0eb4ed2eb
Helena <nocerahelena@gmail.com>
2026-06-20 09:00:19 -0300

    fix: interface

--- protocol.h
+++ protocol.h
@@ -12,19 +12,19 @@
 #define DEFAULT_MAP 2
 
 // Loopback
-/*
+
 #define INTERFACE_SERVER "veth1"
 #define INTERFACE_CLIENT "veth0"
-#define SERVER (unsigned char[]){0x9e, 0x82, 0xe1, 0x6d, 0x4d, 0x6c}
-#define CLIENT (unsigned char[]){0x52, 0x07, 0x95, 0x37, 0x1a, 0xc7}
-*/
+#define SERVER unsigned char[]{0x9e, 0x82, 0xe1, 0x6d, 0x4d, 0x6c}
+#define CLIENT unsigned char[]{0x52, 0x07, 0x95, 0x37, 0x1a, 0xc7}
 
+/*
 //Sofia
 #define INTERFACE_SERVER "enp4s0"
 #define INTERFACE_CLIENT "enx00e04c034558"
 extern unsigned char SERVER[6];
 extern unsigned char CLIENT[6];
-
+*/
 
 typedef enum {
     MSG_ACK = 0,
