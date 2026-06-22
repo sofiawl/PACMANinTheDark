7967a5d18996e23940388a020e558ce689ce45e8
Sofia Lima <sofiawamserl@icloud.com>
2026-06-22 19:34:52 -0300

    adicionei um log do timeout

--- protocol.cpp
+++ protocol.cpp
@@ -191,6 +191,7 @@ int recv_frame(int sock, Frame* f, unsigned char src_mac[6], unsigned char dest_
 
     if (bytes < 0) {
         if (errno == EAGAIN || errno == EWOULDBLOCK) {
+            log("PROTOCOLO", "INFO", "Timeout no recv");
             return -1; // timeout
         }
         log("PROTOCOLO", "ERRO", "Erro no recv");
